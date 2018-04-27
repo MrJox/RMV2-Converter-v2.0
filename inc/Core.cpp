54185be8d4bfa7f01896d91acac9564511ba9bcd
MR.X <salimovhayali@gmail.com>
2018-04-27 19:09:32 +0300

    small fix

--- inc/Core.cpp
+++ inc/Core.cpp
@@ -317,7 +317,7 @@ int Core::Run()
 
 	while (msg.message != WM_QUIT)
 	{
-		if (PeekMessage(&msg, 0, 0, 0, PM_REMOVE))
+		if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
 		{
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
